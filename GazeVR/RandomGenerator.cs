e433223b66134cfdec35ae9388e857054c32c5df
Jesse Geerts <jesse.geerts.14@ucl.ac.uk>
2017-07-14 17:54:43 +0100

    Added RNG and event timestamping from videogame-assay

---
+++ GazeVR/RandomGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeVR
+{
+    public class RandomGenerator : Source<Random>
+    {
+        public int? Seed { get; set; }
+
+        public override IObservable<Random> Generate()
+        {
+            return Observable.Defer(() =>
+            {
+                var seed = Seed;
+                var random = seed.HasValue ? new Random(seed.Value) : new Random();
+                return Observable.Return(random);
+            });
+        }
+    }
+}
---
+++ GazeVR/TimestampedEvent.cs
@@ -0,0 +1,25 @@
+﻿using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeVR
+{
+    public class TimestampedEvent
+    {
+        public TimestampedEvent(DateTimeOffset timestamp, string name, string value)
+        {
+            Timestamp = timestamp;
+            Name = name;
+            Value = value;
+        }
+
+        public DateTimeOffset Timestamp { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
