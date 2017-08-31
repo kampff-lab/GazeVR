fe98d0cb97d822a422d9f151ec3c314ac58505ec
Jesse Geerts <jesse.geerts.14@ucl.ac.uk>
2017-08-31 16:05:15 +0100

    Added custom RandomBoolean operator

---
+++ GazeVR/RandomBoolean.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Bonsai;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeVR
+{
+
+    public class RandomBoolean : Combinator<Random, Boolean>
+    {
+        public override IObservable<bool> Process(IObservable<Random> source)
+        {
+            return source.Select(random =>
+            {
+                return (random.NextDouble() < 0.5);
+            });
+       }
+    }
+
+}
